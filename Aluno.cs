2874205e4b450cc68f2a67935444feccdae460aa
Matheus Yuri <matheusyurirs@gmail.com>
2021-09-10 16:56:30 -0300

    Estudo e treino em CSharp

---
+++ Aluno.cs
@@ -0,0 +1,9 @@
+namespace Estudo
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+
+    }
+}
---
+++ ConceitoEnum.cs
@@ -0,0 +1,13 @@
+namespace Estudo
+{
+    public enum ConceitoEnum
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+
+    }
+}
